a0e3ec21df902c437f98b86d99f6e2584d186b90
zxnlingling <yukizhang0527@outlook.com>
2020-06-23 15:21:14 +0800

    623 权限下拉筛选修复

--- src/main/java/com/jxdinfo/salary/entry/controller/EntryLogController.java
+++ src/main/java/com/jxdinfo/salary/entry/controller/EntryLogController.java
@@ -230,10 +230,45 @@ public class EntryLogController extends BaseController {
         String operationTime = info.get("operationTime");
         Wrapper<EntryLog> ew = new EntityWrapper<>();
 
-        if (!department.equals("")){
-            int departmentId = Integer.parseInt(department);
-            ew.eq("c.DEPARTMENT_ID",departmentId);
+        //权限部分
+        if (currentUser.getPosition().getPositionId()==0){ //当前用户为员工
+            // 根据用户ID查询用户真实权限列表
+            List<Util> permissionList = utilService.selectList((long)currentUser.getStaffId());
+            if (permissionList.size()==0){
+                //当前用户无任何权限
+                System.out.println("您没有查看该日志的权限！");
+                return 0;
+            }
+            if (!department.equals("")){
+                int departmentId = Integer.parseInt(department);
+                ew.eq("c.DEPARTMENT_ID",departmentId);
+            }
+            else {
+                boolean able=false;
+                for (Util p: permissionList){
+                    if(p.getPermissionName().equals("日志查看")){
+                        //该员工用户可以查看新员工录入该部门的日志
+                        able = true;
+                        ew.or().eq("c.DEPARTMENT_ID",p.getDepartmentId());
+                    }
+                }
+                if (!able){//该用户没有日志查看权限
+                    System.out.println("您没有查看该日志的权限！");
+                    return 0;
+                }
+            }
         }
+        else{//当前用户不为员工,可能是部门经理或总经理
+            //如果是总经理，则可以查看全部日志
+            //如果是人力资源部经理，则可以查看全部日志
+            //如果既不是总经理也不是人力资源部经理，则可以访问自己部门录入新员工的日志
+            if(currentUser.getDepartment().getDepartmentId()!=10 &&
+                    currentUser.getDepartment().getDepartmentId()!=99){
+                ew.or().eq("c.DEPARTMENT_ID", currentUser.getDepartment().getDepartmentId());
+            }
+        }
+
+
         if (!position.equals("")){
             int positionId = Integer.parseInt(position);
             ew.eq("c.POSITION_ID",positionId);
--- src/main/java/com/jxdinfo/salary/move/controller/MoveLogController.java
+++ src/main/java/com/jxdinfo/salary/move/controller/MoveLogController.java
@@ -242,10 +242,45 @@ public class MoveLogController extends BaseController {
         String operationTime = info.get("operationTime");
         Wrapper<MoveLog> ew = new EntityWrapper<>();
 
-        if (!oldD.equals("")){
-            int oldDId = Integer.parseInt(oldD);
-            ew.eq("OLD_DEPARTMENT_ID",oldDId);
+        //权限部分
+        if (currentUser.getPosition().getPositionId()==0){ //当前用户为员工
+            // 根据用户ID查询用户真实权限列表
+            List<Util> permissionList = utilService.selectList((long)currentUser.getStaffId());
+            if (permissionList.size()==0){
+                //当前用户无任何权限
+                System.out.println("您没有查看该日志的权限！");
+                return 0;
+            }
+            if (!oldD.equals("")){
+                int oldDId = Integer.parseInt(oldD);
+                ew.eq("OLD_DEPARTMENT_ID",oldDId);
+            }
+            else {
+                boolean able=false;
+                for (Util p: permissionList){
+                    if(p.getPermissionName().equals("日志查看")){
+                        //该员工用户可以查看从该部门调出的日志
+                        able = true;
+                        ew.or().eq("OLD_DEPARTMENT_ID",p.getDepartmentId());
+                    }
+                }
+                if (!able){//该用户没有日志查看权限
+                    System.out.println("您没有查看该日志的权限！");
+                    return 0;
+                }
+            }
         }
+        else{//当前用户不为员工,可能是部门经理或总经理
+            //如果是总经理，则可以查看全部日志
+            //如果是人力资源部经理，则可以查看全部日志
+            //如果既不是总经理也不是人力资源部经理，则可以访问从自己部门调出的日志
+            if(currentUser.getDepartment().getDepartmentId()!=10 &&
+                    currentUser.getDepartment().getDepartmentId()!=99){
+                ew.or().eq("OLD_DEPARTMENT_ID", currentUser.getDepartment().getDepartmentId());
+            }
+        }
+
+
         if (!oldP.equals("")){
             int oldPId = Integer.parseInt(oldP);
             ew.eq("OLD_POSITION_ID",oldPId);
